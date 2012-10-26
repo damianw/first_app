ddb7e95991df0626efaad331431507eb0cb674a6
Damian Wieczorek <damianw@umich.edu>
2012-10-25 23:10:14 -0400

    First commit, yeah nigga

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Othertut::Application.config.secret_token = '51fe0866647ecbe69a131afc12613b3be65165b3747d03cd679884ac5f98f51cf44d6c3c703ec86ec45f34932984a740525d1ce769ebbd3cb422a65ffde3fa26'
